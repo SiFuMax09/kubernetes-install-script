f14a2978c482e9799c4fb6a79a66525d9de7fde1
SiFuMax <me@max-witt.de>
2025-09-04 22:03:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -46,9 +46,9 @@ check_success "Installation von runc"
 # CNI Plugins Installation
 echo -e "\n\033[1;34m--- Installation der CNI Plugins startet ---\033[0m\n"
 mkdir -p /opt/cni/bin
-wget -q --show-progress "https://github.com/containernetworking/plugins/releases/download/v1.7.1/cni-plugins-linux-amd64-v1.7.1.tgz"
+wget -q --show-progress "https://github.com/containernetworking/plugins/releases/download/v1.8.0/cni-plugins-linux-amd64-v1.8.0.tgz"
 check_success "Download der CNI Plugins"
-tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.7.1.tgz
+tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.8.0.tgz
 check_success "Installation der CNI Plugins"
 
 # Kubernetes Installation
