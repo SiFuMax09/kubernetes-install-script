1ea1e76e5e510595fd489baf36ef82074497669a
SiFuMax <me@max-witt.de>
2026-04-01 23:16:51 +0200

    Upgrade Kubernetes APT key and source to v1.35
    Updated Kubernetes APT key and source list for version 1.35.

--- install.sh
+++ install.sh
@@ -127,7 +127,7 @@ apt-get install -y apt-transport-https ca-certificates curl gpg
 
 mkdir -p /etc/apt/keyrings
 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key \
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.35/deb/Release.key \
   | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' \
